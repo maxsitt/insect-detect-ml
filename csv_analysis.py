48cc4acd0e6bb248dba2091fa3e657b53ccc03db
maxsitt <87697856+maxsitt@users.noreply.github.com>
2023-01-19 10:35:47 +0100

    Update csv_analysis.py
    Delete whitespace in description.

--- csv_analysis.py
+++ csv_analysis.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 '''
-Author:       Maximilian Sittinger (https://github.com/maxsitt)
-Website:      https://maxsitt.github.io/insect-detect-docs/
-License:      GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
+Author:   Maximilian Sittinger (https://github.com/maxsitt)
+Website:  https://maxsitt.github.io/insect-detect-docs/
+License:  GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 
 This Python script does the following:
 - read "metadata_classified*.csv" file from current directory into pandas DataFrame
